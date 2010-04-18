532ce0be53c294340aa838e659b675e5b7477619
franck cuny <franck@lumberjaph.net>
2010-04-18 11:28:10 +0200

    initial commit

---
+++ lib/Dancer/Debug.pm
@@ -0,0 +1,33 @@
+package Dancer::Debug;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Dancer::Debug -
+
+=head1 SYNOPSIS
+
+  use Dancer::Debug;
+
+=head1 DESCRIPTION
+
+Dancer::Debug is
+
+=head1 AUTHOR
+
+franck cuny E<lt>franck@lumberjaph.netE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
